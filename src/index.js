9677925d1b2af03ab97db0006cb0ff97ee728e07
Nick <nicknyler@gmail.com>
2025-03-24 09:05:15 +0300

    added server

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const voteForm = document.getElementById("votes-form");
     const resetButton = document.getElementById("reset-btn");
 
+fetch("http://localhost:3000/characters")
+    .then((response) => response.json())
+    .then((characters) => {
+        characters.forEach((character) => {
+            const span = document.createElement("span");
+            span.textContent = character.name;
+            span.style.cursor = "pointer";
+            span.addEventListener("click", () => displayCharacterDetails(character));
+            characterBar.appendChild(span);
+        });
+
+   
